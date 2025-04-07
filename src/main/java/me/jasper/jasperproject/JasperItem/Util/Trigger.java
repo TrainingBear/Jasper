ffc0874c3e1880add22960407156da773d8da7e7
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-04-07 18:06:47 +0700

    sisa lu

--- src/main/java/me/jasper/jasperproject/JasperItem/Util/Trigger.java
+++ src/main/java/me/jasper/jasperproject/JasperItem/Util/Trigger.java
@@ -4,43 +4,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 
-public final class Trigger implements Listener {
-    static final class Interact{
-        static boolean RIGHT_CLICK(Action action, Player player){
+public final class Trigger {
+    public static final class Interact{
+        public static boolean RIGHT_CLICK(Action action, Player player){
             return (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK));
         }
-        static boolean SHIFT_RIGHT_CLICK(Action action, Player player){
+        public static boolean SHIFT_RIGHT_CLICK(Action action, Player player){
             return (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) && player.isSneaking();
         }
-        static boolean RIGHT_CLICK_BLOCK(Action action, Player player){
+        public static boolean RIGHT_CLICK_BLOCK(Action action, Player player){
             return action.equals(Action.RIGHT_CLICK_BLOCK);
         }
-        static boolean SHIFT_RIGHT_CLICK_BLOCK(Action action, Player player){
+        public static boolean SHIFT_RIGHT_CLICK_BLOCK(Action action, Player player){
             return action.equals(Action.RIGHT_CLICK_BLOCK) && player.isSneaking();
         }
-        static boolean RIGHT_CLICK_AIR(Action action, Player player){
+        public static boolean RIGHT_CLICK_AIR(Action action, Player player){
             return action.equals(Action.RIGHT_CLICK_AIR);
         }
-        static boolean SHIFT_RIGHT_CLICK_AIR(Action action, Player player){
+        public static boolean SHIFT_RIGHT_CLICK_AIR(Action action, Player player){
             return action.equals(Action.RIGHT_CLICK_AIR) && player.isSneaking();
         }
 
-        static boolean LEFT_CLICK(Action action, Player player){
+        public static boolean LEFT_CLICK(Action action, Player player){
             return (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK));
         }
-        static boolean SHIFT_LEFT_CLICK(Action action, Player player){
+        public static boolean SHIFT_LEFT_CLICK(Action action, Player player){
             return (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK)) && player.isSneaking();
         }
-        static boolean LEFT_CLICK_BLOCK(Action action, Player player){
+        public static boolean LEFT_CLICK_BLOCK(Action action, Player player){
             return action.equals(Action.LEFT_CLICK_BLOCK);
         }
-        static boolean SHIFT_LEFT_CLICK_BLOCK(Action action, Player player){
+        public static boolean SHIFT_LEFT_CLICK_BLOCK(Action action, Player player){
             return action.equals(Action.LEFT_CLICK_BLOCK) && player.isSneaking();
         }
-        static boolean LEFT_CLICK_AIR(Action action, Player player){
+        public static boolean LEFT_CLICK_AIR(Action action, Player player){
             return action.equals(Action.LEFT_CLICK_AIR);
         }
-        static boolean SHIFT_LEFT_CLICK_AIR(Action action, Player player){
+        public static boolean SHIFT_LEFT_CLICK_AIR(Action action, Player player){
             return action.equals(Action.LEFT_CLICK_AIR) && player.isSneaking();
         }
     }
