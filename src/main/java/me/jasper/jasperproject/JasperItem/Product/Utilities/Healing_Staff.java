ef2f870d0edf3f2a45e4efa08977794b7b0f40b9
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-04-26 14:20:06 +0700

    ketinggalan

--- src/main/java/me/jasper/jasperproject/JasperItem/Product/Utilities/Healing_Staff.java
+++ src/main/java/me/jasper/jasperproject/JasperItem/Product/Utilities/Healing_Staff.java
@@ -10,12 +10,14 @@ import org.bukkit.Material;
 
 public class Healing_Staff extends JItem implements Factory {
     public Healing_Staff(){
-        super("Healing Staff", Material.BREEZE_ROD, Rarity.EPIC, ItemType.STAFF, version, "HEALING_STAFF");
+        super("Healing Staff", Material.BREEZE_ROD, Rarity.EPIC, ItemType.STAFF, "HEALING_STAFF");
+        this.setVersion(getVersion());
         this.setMaxStack(1);
         this.getStats().put(Stats.HEALTH, 75f);
         this.getStats().put(Stats.MANA, 50f);
         this.getStats().put(Stats.DEFENCE, 15f);
         this.getAbilities().add(new Heal(40,2));
+
     }
     @Override
     public JItem create() {
